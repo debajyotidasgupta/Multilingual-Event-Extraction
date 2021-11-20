1295bb2cab6361ce19d2579869a7485affd7c7e4
Somnath Jena <50512475+somnathjena2011@users.noreply.github.com>
2021-11-20 23:34:09 +0530

    tr ch 20

--- models/joint_ee_bert_train_18_10.py
+++ models/joint_ee_bert_train_18_10.py
@@ -1553,9 +1553,9 @@ def train_model(model_id, train_samples, dev_samples, best_model_file):
             # print("PAKISTAN")
 
             print(type(cur_samples_input['positional_seq']))
-            print(type(cur_samples_input['decoder_inpu']))
+            print(type(cur_samples_input['decoder_input']))
             print(type(cur_samples_input['positional_seq'][0]))
-            print(type(cur_samples_input['decoder_inpu'][0]))
+            print(type(cur_samples_input['decoder_input'][0]))
 
             src_words_seq = torch.from_numpy(
                 cur_samples_input['src_words'].astype('long'))  # [23,45,1,56,78,..,0,0,..]

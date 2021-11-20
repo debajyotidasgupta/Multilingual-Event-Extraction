ca324031dacabd7eb8b501fda16bcd82779b40e6
Somnath Jena <50512475+somnathjena2011@users.noreply.github.com>
2021-11-20 22:59:38 +0530

    tr ch 16

--- models/joint_ee_bert_train_18_10.py
+++ models/joint_ee_bert_train_18_10.py
@@ -1534,7 +1534,7 @@ def train_model(model_id, train_samples, dev_samples, best_model_file):
 
             print("cur sample positional seq")
             print(cur_samples_input['positional_seq'])
-            print(len(positional_seq))
+            print(len(cur_samples_input['positional_seq']))
 
             src_words_seq = torch.from_numpy(
                 cur_samples_input['src_words'].astype('long'))  # [23,45,1,56,78,..,0,0,..]
